7ba57110ddb771b8b9484f2f097c9e3a22e8dab2
oranzhang <orancho@outlook.com>
2016-04-28 21:26:56 +0800

    webpack configuration alteration

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports =  {
         {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loaders: [
-          'url-loader',
+          'file',
           'image-webpack?{progressive:true, optimizationLevel: 7, interlaced: false, pngquant:{quality: "65-90", speed: 4}}'
         ]
     },
