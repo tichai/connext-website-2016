2577bc66860c3686524eb26b8e617679d09e39b4
oranzhang <orancho@outlook.com>
2016-04-29 16:08:52 +0800

    button disabled

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -92,8 +92,7 @@ setTimeout(function(){$("#app > div.container.index > div.slogan > div").css("op
             <li>我们提供了 23 张免费门票。介绍自己并告诉我们你对本次活动的期待，写信给 <a href="mailto:free16@connext.im">free16@connext.im</a> 争取一个名额。</li>
           </ul>
         </blockquote>
-        <p class="aller"><a href="#"><i class="fa fa-angle-double-right" aria-hidden="true"></i>
- 跳转到活动行</a></p>
+        <p class="aller disabled"><a href="javascript:;"><i class="fa fa-angle-double-right" aria-hidden="true"></i>跳转到活动行</a></p>
       </div>
       <h3 style="display: none;">赞助商</h3>
     </div>
@@ -107,6 +106,158 @@ setTimeout(function(){$("#app > div.container.index > div.slogan > div").css("op
   100% {
     opacity: 1;
   }
+}
+@keyframes shakee {
+2% {
+    transform: translate(1px, 0) rotate(0);
+}
+4% {
+    transform: translate(4px, 0) rotate(0);
+}
+6% {
+    transform: translate(7px, 0) rotate(0);
+}
+8% {
+    transform: translate(-1px, 0) rotate(0);
+}
+10% {
+    transform: translate(-1px, 0) rotate(0);
+}
+12% {
+    transform: translate(1px, 0) rotate(0);
+}
+14% {
+    transform: translate(-3px, 0) rotate(0);
+}
+16% {
+    transform: translate(-8px, 0) rotate(0);
+}
+18% {
+    transform: translate(-9px, 0) rotate(0);
+}
+20% {
+    transform: translate(-3px, 0) rotate(0);
+}
+22% {
+    transform: translate(-9px, 0) rotate(0);
+}
+24% {
+    transform: translate(1px, 0) rotate(0);
+}
+26% {
+    transform: translate(10px, 0) rotate(0);
+}
+28% {
+    transform: translate(9px, 0) rotate(0);
+}
+30% {
+    transform: translate(-2px, 0) rotate(0);
+}
+32% {
+    transform: translate(9px, 0) rotate(0);
+}
+34% {
+    transform: translate(2px, 0) rotate(0);
+}
+36% {
+    transform: translate(-5px, 0) rotate(0);
+}
+38% {
+    transform: translate(3px, 0) rotate(0);
+}
+40% {
+    transform: translate(0px, 0) rotate(0);
+}
+42% {
+    transform: translate(4px, 0) rotate(0);
+}
+44% {
+    transform: translate(-7px, 0) rotate(0);
+}
+46% {
+    transform: translate(-4px, 0) rotate(0);
+}
+48% {
+    transform: translate(9px, 0) rotate(0);
+}
+50% {
+    transform: translate(-6px, 0) rotate(0);
+}
+52% {
+    transform: translate(-3px, 0) rotate(0);
+}
+54% {
+    transform: translate(3px, 0) rotate(0);
+}
+56% {
+    transform: translate(6px, 0) rotate(0);
+}
+58% {
+    transform: translate(-5px, 0) rotate(0);
+}
+60% {
+    transform: translate(-6px, 0) rotate(0);
+}
+62% {
+    transform: translate(8px, 0) rotate(0);
+}
+64% {
+    transform: translate(8px, 0) rotate(0);
+}
+66% {
+    transform: translate(-7px, 0) rotate(0);
+}
+68% {
+    transform: translate(8px, 0) rotate(0);
+}
+70% {
+    transform: translate(6px, 0) rotate(0);
+}
+72% {
+    transform: translate(5px, 0) rotate(0);
+}
+74% {
+    transform: translate(6px, 0) rotate(0);
+}
+76% {
+    transform: translate(7px, 0) rotate(0);
+}
+78% {
+    transform: translate(-2px, 0) rotate(0);
+}
+80% {
+    transform: translate(1px, 0) rotate(0);
+}
+82% {
+    transform: translate(0px, 0) rotate(0);
+}
+84% {
+    transform: translate(-6px, 0) rotate(0);
+}
+86% {
+    transform: translate(0px, 0) rotate(0);
+}
+88% {
+    transform: translate(1px, 0) rotate(0);
+}
+90% {
+    transform: translate(-1px, 0) rotate(0);
+}
+92% {
+    transform: translate(1px, 0) rotate(0);
+}
+94% {
+    transform: translate(4px, 0) rotate(0);
+}
+96% {
+    transform: translate(1px, 0) rotate(0);
+}
+98% {
+    transform: translate(3px, 0) rotate(0);
+}
+0%, 100% {
+    transform: translate(0, 0) rotate(0);
+}
 }
 .tianyu {
   background: #0A4356;
@@ -226,6 +377,28 @@ setTimeout(function(){$("#app > div.container.index > div.slogan > div").css("op
     .aller {
       margin-top: 3rem;
       text-align: center;
+      &.disabled {
+        &:after {
+            position: absolute;
+            content: "暂时不可用";
+            font-size: 1.2rem;
+            color: #156276;
+            left: 0;
+            width: 100%;
+            transform: translateY(5.5rem);
+          }
+        cursor: not-allowed;
+        a {
+          background: rgb(17, 79, 95);
+          box-shadow: 0 0 8px rgba(0, 0, 0, 0.67) inset;
+          cursor: not-allowed;
+          color: #A2A2A2 !important;
+          &:hover {
+
+            color: #A2A2A2 !important;
+          }
+        }
+      }
       a {
         color: #222 !important;
         background: #eee;
--- src/components/IFooter.vue
+++ src/components/IFooter.vue
@@ -5,7 +5,10 @@
 </script>
 
 <template>
-<p class="footer">本站不保证在老旧设备上的体验.<br />Copyright under MIT licence. <br />Designed by Awc, made with <i class="fa fa-heart"></i>. </p>
+<p class="footer">本站不保证在老旧设备以及腾讯X5内核上的体验.<br />
+但保证在 Windows Phone 8.1 以及 Windows 10 Mobile 上的体验.<br />
+Copyright under MIT licence. <br />
+Designed by Awc, made with <i class="fa fa-heart"></i>. </p>
 </template>
 
 <style lang="stylus">
